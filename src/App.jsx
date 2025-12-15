f4891e4fad8a39e7248710717e522c39750a24a8
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-12-15 15:18:41 +0500

    yes full

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
 import { Footer } from './compnents/footer'
 import './App.css'
 import Header from './compnents/header'
 import Main from './compnents/main'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+ <>
+ <a className='text-2xl text-center pl-52 text-red-500 bg-black' href="https://github.com/UMER-FAROOQ-attari/lab-project">github LINk</a>
 <Header />
 <Main />
     <Footer />
