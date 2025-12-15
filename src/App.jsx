bdbbe68382fb40426dec44afddbbe0c3bf1d45ab
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-12-15 15:40:28 +0500

    soorry

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Footer from './components/Footer'
-import Header from './components/Header'
-import Main from './components/Main'
+import Footer from './compnents/Footer'
+import Header from './compnents/Header'
+import Main from './compnents/Main'
 import './App.css'
 
 function App() {
